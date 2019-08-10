162d26ab4ab77ba730cab4025b7f8fff4cfbb789
Simon Delberghe <open-source@orandin.fr>
2019-08-10 17:27:36 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,21 @@
 var path = require('path')
 const CopyPlugin = require('copy-webpack-plugin')
+const webpack = require('webpack')
 const fs = require('fs')
 const SvgoInstance = require('svgo')
 
 const entry = require('./package.json').main
 
-const svgo = new SvgoInstance()
+const readManifest = () =>
+  JSON.parse(fs.readFileSync(path.join(__dirname, './manifest.konnector')))
+
+const svgo = new SvgoInstance({
+  plugins: [
+    {
+      inlineStyles: { onlyMatchedOnce: false }
+    }
+  ]
+})
 
 let iconName
 try {
@@ -25,18 +35,6 @@ module.exports = {
     path: path.join(__dirname, 'build'),
     filename: 'index.js'
   },
-  module: {
-    rules: [
-      {
-        test: /\.node$/,
-        use: [
-          {
-            loader: 'node-loader'
-          }
-        ]
-      }
-    ]
-  },
   plugins: [
     new CopyPlugin([
       { from: 'manifest.konnector' },
@@ -45,8 +43,18 @@ module.exports = {
       { from: 'assets', transform: optimizeSVGIcon },
       { from: '.travis.yml' },
       { from: 'LICENSE' }
-    ])
-  ]
+    ]),
+    new webpack.DefinePlugin({
+      __WEBPACK_PROVIDED_MANIFEST__: JSON.stringify(readManifest())
+    })
+  ],
+  module: {
+    // to ignore the warnings like :
+    // WARNING in ../libs/node_modules/bindings/bindings.js 76:22-40
+    // Critical dependency: the request of a dependency is an expression
+    // Since we cannot change this dependency. I think it won't hide more important messages
+    exprContextCritical: false
+  }
 }
 
 function optimizeSVGIcon(buffer, path) {
